85fe2bd39e72112a4bcd0fce4d268321b2ee011f
rilaby <mailtorila@gmail.com>
2017-08-29 23:51:39 +0300

    Proxy design pattern example added

---
+++ gs-dp/proxy/src/main/java/com/rilaby/proxy/IvoryTower.java
@@ -0,0 +1,14 @@
+package com.rilaby.proxy;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class IvoryTower implements WizardTower {
+
+  private static final Logger logger = LoggerFactory.getLogger(IvoryTower.class);
+
+  public void enter(Wizard wizard) {
+    logger.info("{} enters the tower.", wizard);
+  }
+
+}
---
+++ gs-dp/proxy/src/main/java/com/rilaby/proxy/WizardTower.java
@@ -0,0 +1,6 @@
+package com.rilaby.proxy;
+
+public interface WizardTower {
+
+  void enter(Wizard wizard);
+}
